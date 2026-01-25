8a8255ac1a524c240b0151293e76208b7fef88db
JACKIE-AGB <donnovancreano2003@gmail.com>
2026-01-24 20:54:23 -0700

    No se pudo hacer hoy

--- proyecto_ETD/lang.py
+++ proyecto_ETD/lang.py
@@ -1,23 +1,2 @@
-import transformers
-import torch
-
-model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
-
-pipeline = transformers.pipeline (
-    "text-generation",
-    model=model_id,
-    model_kwargs={"torch_dtype": torch.bfloat16},
-    device_map="auto",
-)
-
-messages = [
-    {"role": "syste,", "content": "You are a pirate chatbot who always responds in pirate speak!!!"},
-    {"role": "user", "content": "Who are you?"},
-]
-
-outputs = pipeline(
-    messages,
-    max_new_token=256,
-)
-
-print(outputs[0]["generated_text"][-1])
+from transformers import pipeline
+from langchain_huggingface import HuggingFacePipeline
