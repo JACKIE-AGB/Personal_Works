569e920ac2fc35bd398c38b21da28f46d353e3da
Maghrabi-svg <donnovancreano2003@gmail.com>
2026-01-28 20:54:05 -0700

    aksjdkasd

--- proyecto_ETD/files.py
+++ proyecto_ETD/files.py
@@ -1,31 +1,15 @@
-import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+from transformers import pipeline
 
-model_id = "Qwen/Qwen3-Coder-30B-A3B-Instruct"
-
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_compute_dtype=torch.bfloat16,
-    bnb_4bit_quant_type="nf4"
+text = pipeline(
+    "text-generation",
+    model="mistralai/Mistral-7B-Instruct-v0.1",
+    device=0
 )
 
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModelForCausalLM.from_pretrained(
-    model_id,
-    quantization_config=bnb_config,
-    device_map="auto",
-    trust_remote_code=True
+text(
+    "explain what artifical intelligence is in spanish",
+    max_new_tokens = 150,
+    do_sample=True,
+    temperature=0.7,
+    top_p=0.9
 )
-
-messages = [
-    {"role": "system", "content": "Eres un experto programador senior que escribio un codigo limpio y eficiente"},
-    {"role": "user", "content": "¿Como puedo implementear un middleware de autenticacion JWT en Node.js?"}
-]
-
-text = tokenizer.apply_chat_template(messages, tokenizer=False, add_generation_prompt=True)
-model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
-
-generated_ids = model.generate(model_inputs.inputs_ids, max_new_tokens=1024)
-response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-
-print(response)
